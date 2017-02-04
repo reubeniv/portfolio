f52233c7207a034f5b5dbc0cfec6ebf628d8122d
reubeniv <s_munsey@live.com>
2017-02-04 17:29:48 +0530

    onblur now only activates on individual input elements

--- resources/demos/formvalid/formvalid-script.js
+++ resources/demos/formvalid/formvalid-script.js
@@ -5,7 +5,11 @@
         alert("failed");
     });*/
 
-var valid = 0;
+var valid = 1;
+var submit_flag = 0;
+var errorMessage = "";
+var messageStart = "<ul>"
+var messageEnd = "</ul>"
 
 function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -16,16 +20,9 @@ function isPhone(phone) {
     return $.isNumeric(phone);
 }
 
-var validate = function(){
-    valid = 1;
-    var errorMessage = "";
-    var messageStart = "<ul>"
-    var messageEnd = "</ul>"
+var validate_email = function(){
 
     var email = $("#email").val().trim();
-    var phone = $("#phone").val().trim();
-    var pass = $("#password").val().trim();
-    var conf = $("#confirm").val().trim();
 
     if(!isEmail(email) || email == ""){
         $("#email").css("background-color", "pink");
@@ -34,6 +31,10 @@ var validate = function(){
     }else{
         $("#email").css("background-color", "#77dd77");
     }
+}
+
+var validate_phone = function(){
+    var phone = $("#phone").val().trim();
 
     if(!isPhone(phone) || phone == ""){
         $("#phone").css("background-color", "pink");
@@ -42,6 +43,10 @@ var validate = function(){
     }else{
         $("#phone").css("background-color", "#77dd77");
     }
+}
+
+var validate_pass = function(){
+    var pass = $("#password").val().trim();
 
     if(pass == ""){
         $("#password").css("background-color", "pink");
@@ -51,6 +56,11 @@ var validate = function(){
         $("#password").css("background-color", "#77dd77");
         $("#confirm").css("background-color", "#77dd77");
     }
+}
+
+var validate_conf = function(){
+    var conf = $("#confirm").val().trim();
+    var pass = $("#password").val().trim();
 
     if(pass != conf){
         $("#confirm").css("background-color", "pink");
@@ -58,24 +68,50 @@ var validate = function(){
         valid = 0;
     }
 
+    return valid;
+}
+
+var validate_all = function(){
+    valid = 1;
+
+    validate_email();
+    validate_phone();
+    validate_pass();
+    validate_conf();
+
     if(valid == 1){
-        $(".error-message").html("<p>form is valid</p>").css({"background-color": "#77dd77", "border-color": "green"}).show();
+        $(".error-message").html("<p>form is valid</p>").css({"background-color": "#77dd77", "border-color": "green", "padding-top": "12px"}).show();
 
         //alert("form is valid");
     }else{
         errorMessage = messageStart + errorMessage + messageEnd;
-        $(".error-message").html(errorMessage).css({"background-color": "pink", "border-color": "red"}).show();
+        $(".error-message").html(errorMessage).css({"background-color": "pink", "border-color": "red", "padding-top": "12px"}).show();
 
     }
 }
 
 
 $("input").blur(function(){
-    validate();
+
+    var input_id = $(this).attr('id')
+    errorMessage = "";
+
+    if(input_id == "email"){
+        validate_email();
+    }else if(input_id == "phone"){
+        validate_phone();
+    }else if(input_id == "pass"){
+        validate_pass();
+    }else if(input_id == "conf"){
+        validate_conf();
+    }
+
 });
 
 $("#submit").click(function(){
-    validate();
+
+    errorMessage = "";
+    validate_all();
 
     if(valid === 1){
         alert("This form doesn't really submit anything, but if it was wehey the fields are valid");
